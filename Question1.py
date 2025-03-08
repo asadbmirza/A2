708149054191f0a5937c63a205f2ad91fb9bb71d
asadbmirza <asadbmirza97@gmail.com>
2025-03-08 15:05:35 -0500

    feat: added 1.2 and 1.3

--- Question1.py
+++ Question1.py
@@ -2,8 +2,46 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/emoji/<name>')
+def emoji(name: str):
+    formattedName = formatEmojiName(name)
+
+    return formatResponse(formattedName)
+
+def formatEmojiName(name):
+    emoji_dict = {"A": "🔺", "E": "🎗", "I": "👁", "O": "🔵", "U": "🆙"}
+
+    formattedEmojiName = ""
+    for char in name:
+
+        if char.upper() in emoji_dict:
+            formattedEmojiName += emoji_dict[char.upper()]
+        else:
+            formattedEmojiName += char
+    return formattedEmojiName
+
 @app.route('/<name>')
 def name(name: str):
+    formattedName = formatName(name)
+
+    return formatResponse(formattedName)
+
+def formatResponse(name: str):
+    response = "Welcome, " + name + ", "
+    endText = "to my CSCB20 website!"
+    if isPalindrome(name):
+        endText = "Your name is a palindrome!"
+
+    return response + endText
+
+def isPalindrome(text: str):
+    length = len(text) - 1
+    for i in range(length // 2):
+        if text[i] != text[length - i]:
+            return False
+    return True
+
+def formatName(name: str):
     formattedName = name.strip()
     print(formattedName)
     if hasDigit(formattedName):
@@ -14,8 +52,7 @@ def name(name: str):
         formattedName = name.upper()
     else:        
         formattedName = formattedName.title()
-
-    return "Welcome, " + formattedName + ", to my CSCB20 website!"
+    return formattedName
 
 def removeNumbers(name: str):
     formattedName = ''
